f57d8d34389207f63963c0bbc0f363d37be74a0c
Bao Vo <giabaovo123456@gmail.com>
2024-05-19 11:16:30 +0700

    Add employer register API

--- authentication/views.py
+++ authentication/views.py
@@ -2,7 +2,8 @@ from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 
 from authentication.serializers import (
-    JobSeekerRegisterSerializer
+    JobSeekerRegisterSerializer,
+    EmployerRegisterSerializer
 )
 
 from helpers import helper
@@ -23,5 +24,22 @@ def job_seeker_register(request):
             helper.send_verify_email(request, user)
     except Exception as ex:
         helper.print_log_error('job_seeker_register', ex)
+        return var_res.response_data(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    return var_res.response_data(status=status.HTTP_201_CREATED)
+
+@api_view(http_method_names=['post'])
+def employer_register(request):
+    data = request.data
+    serializer = EmployerRegisterSerializer(data=data)
+    if not serializer.is_valid():
+        return var_res.response_data(status=status.HTTP_400_BAD_REQUEST, errors=serializer.errors)
+    
+    try:
+        user = serializer.save()
+
+        if user:
+            helper.send_verify_email(request, user)
+    except Exception as ex:
+        helper.print_log_error('employer_register', ex)
         return var_res.response_data(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     return var_res.response_data(status=status.HTTP_201_CREATED)
